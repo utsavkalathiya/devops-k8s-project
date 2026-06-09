d306b2ef699b13d176db9f14b98c8d57e61eea0c
utsavXkalathiya <utsavkalathiya811@gmail.com>
2026-06-09 17:35:46 -0400

    file updated:app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def home():
     environment = os.getenv("ENVIRONMENT", "Development")
     hostname = socket.gethostname()
     
-    return f"<h1>Automated Factory is Online! and working fine</h1><p>Environment: {environment}</p><p>Running on Pod/Container ID: {hostname}</p>"
+    return f"<h1>Automated Factory is Online!</h1><p>Environment: {environment}</p><p>Running on Pod/Container ID: {hostname}</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
